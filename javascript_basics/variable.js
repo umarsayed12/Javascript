ff362c0b7914b767f2f1271a7811dbffbc1e896e
Umar Khursheed <137692710+umarsayed12@users.noreply.github.com>
2024-02-22 21:33:21 +0000

    variable.js file added

---
+++ javascript_basics/variable.js
@@ -0,0 +1,11 @@
+const id = 12344
+var holder = "Umar Sayed"
+let email = "usayed156@gmail.com"
+age = 18
+
+/*
+1. var keyword should not be used. Instead use let.
+2. const value cannot be changed.
+3. table can be used to print all data together.
+*/
+console.table([id,holder,email,age])

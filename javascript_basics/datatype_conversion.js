584c7c605a1f7fbf2407d4a7f9216db126d33329
Umar Khursheed <137692710+umarsayed12@users.noreply.github.com>
2024-02-28 19:14:55 +0000

    Added datatype_coversion.js

---
+++ javascript_basics/datatype_conversion.js
@@ -0,0 +1,21 @@
+let marks = 234
+marksString = String(marks)
+console.log(marksString)
+console.log(typeof marksString)
+
+stringValue = "umar"
+boolenValue = Boolean(stringValue)
+console.log(boolenValue);  // => true
+
+newbooleanValue = true
+newstringValue = String(newbooleanValue)
+console.log(newstringValue); // => "true"
+
+newNumber = 0
+newBool = Boolean(newNumber)
+console.log(newBool);
+
+undef = "abc"
+num = Number(undef)
+console.log(num);    // => NaN (Not a Number)
+console.log(typeof num); // => number
